ce89713d770928a57f07b420315aa97529ee8be9
HsinWong <594968184@qq.com>
2018-04-12 16:41:24 +0800

    Initial commit

--- src/main/java/com/hsinwong/cms/repository/MenuRepository.java
+++ src/main/java/com/hsinwong/cms/repository/MenuRepository.java
@@ -1,11 +1,20 @@
 package com.hsinwong.cms.repository;
 
 import com.hsinwong.cms.bean.Menu;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
-public interface MenuRepository extends PagingAndSortingRepository<Menu, Long> {
+public interface MenuRepository extends CrudRepository<Menu, Long> {
 
     List<Menu> findByState(Menu.State state);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Menu entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Long aLong);
 }
--- src/main/java/com/hsinwong/cms/repository/UserRepository.java
+++ src/main/java/com/hsinwong/cms/repository/UserRepository.java
@@ -2,10 +2,19 @@ package com.hsinwong.cms.repository;
 
 import com.hsinwong.cms.bean.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(User entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Long aLong);
 }
